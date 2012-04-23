36616bdcdcbcbdf9b39487e41e6f55fc76873e33
Danny Joris <danny_joris@yahoo.co.uk>
2012-04-23 11:44:06 -0300

    link columns

---
+++ js/script.js
@@ -0,0 +1,7 @@
+
+Drupal.behaviors.linkColumns = function(context) {
+
+  // double links column styling
+  $('.field-field-canned-search-links-right').prev('.field-field-canned-search-links').addClass('links-first-column');
+
+}
